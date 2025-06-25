ee85860e3ea0da9c0986f82cb2a0a08c6e668f06
Slacker201 <slacker1234567809@gmail.com>
2025-06-25 10:15:18 -0600

    Added random item selection by rarity

--- src/heroguy_gatcha/cmd_gatcha_draw.rs
+++ src/heroguy_gatcha/cmd_gatcha_draw.rs
@@ -1,7 +1,7 @@
 
 use serenity::{all::{CommandInteraction, Context, GuildId, Interaction}, builder};
 
-use crate::{get_item_name_from_id, get_user_cache};
+use crate::{data_management::item_manager, get_user_cache};
 
 
 
@@ -17,21 +17,27 @@ pub async fn load_command(context: &Context) {
 pub async fn run_command(cmd: &CommandInteraction, context: Context, _interaction: &Interaction) {
     println!("Recieved draw request from user {}", cmd.user.id);
     let mut cache = get_user_cache().await;
-    let content;
+    let drawn_item_name;
     let count;
+    let mut chosen_item_ids = Vec::new();
     match cache.get_user_data(cmd.user.id.get()) {
         Some(mut user_data) => {
-            user_data.add_item(1, 1);
-            content = get_item_name_from_id(1);
+            let rarity_entries = user_data.gatcha_chance.draw_item_rarities().await;
+            for rarity_entry in rarity_entries {
+                for _ in 0..rarity_entry.0 {
+                    chosen_item_ids.push(item_manager::pick_random_item_by_rarity(rarity_entry.1.clone()));
+                }
+            }
+            drawn_item_name = "idk".to_string();
             count = 1;
         },
         None => {
-            content = "Invalid Item".to_string();
+            drawn_item_name = "Invalid Item".to_string();
             count = 0
         },
     }
     let err = cmd.create_response(context, builder::CreateInteractionResponse::Message(builder::CreateInteractionResponseMessage::new()
-            .content(format!("Drew {} {}", count, content)).ephemeral(true))).await;
+            .content(format!("Drew {} {}", count, drawn_item_name)).ephemeral(true))).await;
     if let Err(error) = err {
         println!("Error sending message: {:?}", error);
     }
